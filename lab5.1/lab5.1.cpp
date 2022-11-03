77e3601e818b47f4efa4441d2a9694aaa30dde13
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-11-04 00:54:39 +0200

    5.0

---
+++ lab5.1/lab5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double g(const double a, const double b, const double c); // прототип
+int main()
+{
+    double x, y;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    double c = 1. * (g(x * x, 1., y) - g(y * y, x, 1.)) / (1 + g(sqrt(x), y, 1.));
+    cout << "c = " << c << endl;
+    return 0;
+}
+double g(const double a, const double b, const double c) // визначення
+{
+    return (a * a + sin(b) + 1) / (1 + c * c);
+}
