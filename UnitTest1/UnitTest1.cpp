e18ffa4668be7f67d735132ecd7673f14e438d8b
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-11-04 01:01:41 +0200

    lab05

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.1/lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = g(1, 0, 2);
+			Assert::AreEqual(0.4, t, 0.00001);
+
+		}
+	};
+}
